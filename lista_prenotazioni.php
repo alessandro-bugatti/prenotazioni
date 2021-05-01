ef2092087606e6f35a68d42ff68e1ef369127a3a
alessandro-bugatti <alessandro.bugatti@gmail.com>
2021-05-01 11:05:42 +0200

    Improve code

--- lista_prenotazioni.php
+++ lista_prenotazioni.php
@@ -8,19 +8,21 @@ use League\Plates\Engine;
 //Viene creato l'oggetto per la gestione dei template
 $templates = new Engine('./view','tpl');
 
-//Query per recuperare tutte le prenotazioni
-$sql = "SELECT * FROM prenotazioni";
 
-//Invio la query al server MySQL
-$stmt = $pdo->query($sql);
-
-//Estraggo le righe di risposta che finiranno come vettori in $result
-$result = $stmt->fetchAll();
 
 //Se sei una persona che ha fatto il login
 if (isset($_SESSION['username']))
 {
     $username = $_SESSION['username'];
+
+    //Query per recuperare tutte le prenotazioni
+    $sql = "SELECT * FROM prenotazioni";
+
+    //Invio la query al server MySQL
+    $stmt = $pdo->query($sql);
+
+    //Estraggo le righe di risposta che finiranno come vettori in $result
+    $result = $stmt->fetchAll();
 
     //Rendo un template che mi visualizza la tabella
     echo $templates->render('lista_prenotazioni',
